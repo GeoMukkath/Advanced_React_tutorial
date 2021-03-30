d093c3cd7d96bbace3593676cb686fde3935aa50
GeoMukkath <geomukkath@yandex.com>
2021-03-30 10:21:05 +0530

    Added useSate hook with toggle

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let title = "Random title";
+  const handleClick = () => {
+    title = "Hello Geo";
+    console.log(title);
+  };
+  return (
+    <>
+      <h2>{title}</h2>
+      <button type="button" className="btn" onClick={handleClick}>
+        Change title
+      </button>
+    </>
+  );
 };
 
 export default ErrorExample;
--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [text, setText] = useState("Random title");
+  const handleClick = () => {
+    if (text === "Random title") {
+      setText("hello world");
+    } else {
+      setText("Random title");
+    }
+  };
+  return (
+    <>
+      <h2>useState example</h2>
+      <h3>{text}</h3>
+      <button className="btn" onClick={handleClick}>
+        Change title
+      </button>
+    </>
+  );
 };
 
 export default UseStateBasics;
