5509d222a909c53afb8751c588485b81c814523d
GeoMukkath <geomukkath@yandex.com>
2021-03-31 00:36:27 +0530

    useState completed and added counter exercise

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = useState(0);
+  const upByOne = () => {
+    setValue(value + 1);
+  };
+  const downByOne = () => {
+    setValue(value + -1);
+  };
+  return (
+    <>
+      <section>
+        <h1>Regular counter</h1>
+        <h3>{value}</h3>
+        <button type="button" className="btn" onClick={upByOne}>
+          +1
+        </button>
+        <button type="button" className="btn" onClick={() => setValue(0)}>
+          Reset
+        </button>
+        <button type="button" className="btn" onClick={downByOne}>
+          -1
+        </button>
+      </section>
+    </>
+  );
 };
 
 export default UseStateCounter;
