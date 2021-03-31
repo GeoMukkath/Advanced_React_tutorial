8c7971752f5ba26c4a17982aa520c836d0b443f9
GeoMukkath <geomukkath@yandex.com>
2021-03-31 11:47:42 +0530

    Started with cleanup in useEffect

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // cleanup function
 // second argument
-
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+  return (
+    <>
+      <h2>Window</h2>
+      <h3>{size}</h3>
+    </>
+  );
 };
 
 export default UseEffectCleanup;
