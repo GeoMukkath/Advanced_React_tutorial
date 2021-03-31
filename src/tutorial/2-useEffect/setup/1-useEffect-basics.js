01fbf7e5b547ce1d4437b7b2798399adc80db0a5
GeoMukkath <geomukkath@yandex.com>
2021-03-31 10:17:32 +0530

    Use Effect basics

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+  const upByOne = () => {
+    setValue(value + 1);
+  };
+  useEffect(() => {
+    console.log("call useEffect");
+  });
+  console.log("render component");
+  return (
+    <>
+      <h3>{value}</h3>
+      <button type="button" className="btn" onClick={upByOne}>
+        +1
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
